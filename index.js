3cfcd8b48bf1833951bc40584cdc113647b8f49f
Charles Sin <d0561910@mail.fcu.edu.tw>
2019-11-30 15:55:38 +0800

    3

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 console.log("HELLO WORLD");
 console.log("Testing");
 console.log("delete console.log()");
-<<<<<<< HEAD
 console.log("second");
-=======
 console.log("Adding new lines");
->>>>>>> cb8ecc68f04a33df19db954938be8e01ae226764
